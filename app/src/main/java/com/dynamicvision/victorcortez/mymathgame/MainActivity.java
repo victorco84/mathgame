3a8927c9a3732889bd2a8cf6d57e517e4bda08b4
victoco84 <victor@dynamicviz.com>
2016-10-24 22:47:36 -0700

    Simple Android Math Game

---
+++ app/src/main/java/com/dynamicvision/victorcortez/mymathgame/MainActivity.java
@@ -0,0 +1,39 @@
+package com.dynamicvision.victorcortez.mymathgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int     a = 10;
+        String  b = "Alan Turing";
+        boolean c = false;
+
+        Log.i("info","info a=" + a);
+        Log.i("info","info b=" + b);
+        Log.i("info","info c=" + c);
+
+        a++;
+        a = a + 10;
+        b = b + " was smarter than the average bear Booboo";
+        b = b + a;
+        c = (1 + 1 == 3);//1 + 1 is definitely 2! So false.
+
+
+        Log.i("info","info a=" + a);
+        Log.i("info","info b=" + b);
+        Log.i("info","info c=" + c);
+
+
+    }
+
+
+}
